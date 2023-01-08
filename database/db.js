2f748fdeffa34ce0a589d8cbe68b3fe1ddc85e28
Marshal <olmarsh.192@gmail.com>
2023-01-08 15:24:37 +0100

    refactored the connection to DB code

--- database/db.js
+++ database/db.js
@@ -1,25 +1,10 @@
-const mongoose = require('mongoose');
-require("dotenv").config();
+const mongoose = require('mongoose')
 
-const CONNECTION_STRING = process.env.CONNECTION_STRING
-
-
-function connectToDB() {
-    mongoose.connect(CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-    
-    mongoose.connection.on("connected", () => {
-        console.log("Successfully connected to Database");
-    });
-
-    mongoose.connection.on("error", (err) => {
-        console.log("An error occured");
-        console.log(err);
-    });
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 }
 
-module.exports = {
-    connectToDB
-};
+module.exports = connectDB
